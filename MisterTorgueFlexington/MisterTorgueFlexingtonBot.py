64dbedce97da03317594c1693215c51c9f3f8d3e
Nathaniel Stepp <stepp.nathaniel@gmail.com>
2019-10-01 21:01:08 -0500

    Updated bot code, swapping the second and thrid entries in the on_message if-statement tree. By placing the BDE entry before the general catch phrase entry, the 'EXPLOSIONS' in 'BDE EXPLOSIONS' won't accidently enter the random quote sub-routine before the BDE sub-routine.

--- MisterTorgueFlexington/MisterTorgueFlexingtonBot.py
+++ MisterTorgueFlexington/MisterTorgueFlexingtonBot.py
@@ -15,11 +15,11 @@
 import os
 import random
 import discord
+import asyncio
 from dotenv import load_dotenv
 from MisterTorgueFlexingtonCatchPhrases import catchPhrases
 from MisterTorgueFlexingtonQuotes import torgueQuotes
 from MisterTorgueFlexingtonPlays import torguePlaying
-import asyncio
 
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
@@ -36,28 +36,28 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-    
-    if any(x in message.content for x in catchPhrases):
-        response = random.choice(torgueQuotes)
-        await message.channel.send(response)
-        
-    elif message.content.startswith('BDE'):
+		
+    if message.content.startswith('BDE'):
         bde_item = message.content
         if bde_item[4:] == "EXPLOSIONS":
             await message.channel.send("EXPLOSIONS ARE ALWAYS 100% BDE!!!!")
         else:
             bde_response = "%s IS NO MORE THAN %d%% BDE MEEDLY MOOOOW-WOW-WOW" %(bde_item[4:], random.randint(0, 100))
             await message.channel.send(bde_response)
+			
+    elif any(x in message.content for x in catchPhrases):
+        response = random.choice(torgueQuotes)
+        await message.channel.send(response)
         
     elif message.content == 'raise-exception':
         raise discord.DiscordException
-
+		
 async def my_background_task():
     await client.wait_until_ready()
     while not client.is_closed():
         game = discord.Game(torguePlaying())
         await client.change_presence(status = discord.Status.online, activity = game);
-        await asyncio.sleep(14400)      #4 hours
+        await asyncio.sleep(14400) #4 hours
         
 client.loop.create_task(my_background_task())
 client.run(token)
